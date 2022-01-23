7a04f0336d54b2c61a4b9c6b9d43e04c6ed5554a
victorFresneda <vafresnedah@hotmail.com>
2022-01-22 23:48:21 -0500

    Se utiliza el memo en una aplicacion real

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import ReactDOM from 'react-dom';
 //import { FormWithCustimHook } from './components/02-useEffect/FormWithCustimHook';
 //import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 //import { FocusScreen } from './components/04-useRef/FocusScreen';
-import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+//import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+//import { Layout } from './components/05-useLayoutEffect/Layout';
+import { Memorize } from './components/06-memos/Memorize';
 //import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 //import { SimpleForm } from './components/02-useEffect/SimpleForm';
 //import { CounterApp } from './components/01-useState/CounterApp';
@@ -15,7 +17,7 @@ import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 
 ReactDOM.render(
   
-    <RealExampleRef />,
+    <Memorize />,
   
   document.getElementById('root')
 );
